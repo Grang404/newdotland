8ee6418168220c2e7b4f513a8a10e6b77ba362ce
Grang404 <chunter404@proton.me>
2025-12-04 22:31:39 +0000

    owah

--- generate_configs.sh
+++ generate_configs.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTFILES_DIR="$SCRIPT_DIR"
 
 # Source detection
-# source "$DOTFILES_DIR/detect.sh"
-source "$DOTFILES_DIR/test.sh"
+source "$DOTFILES_DIR/detect.sh"
+# source "$DOTFILES_DIR/test.sh"
 
 declare -A MONITOR_INFO
 
@@ -33,49 +33,67 @@ generate_monitor_config() {
 generate_workspace_config() {
     local config=""
 
-    echo "Workspace configuration"
-    echo "1) Groob"
-    echo "2) Sol"
-    read -rp $'Pick an option\n' choice
+    if [[ $MONITOR_COUNT -eq 1 ]]; then
+        for i in {1..10}; do
+            config+="workspace = $i"$'\n'
+        done
 
-    # Groob config
-    if [[ $choice -eq 1 ]]; then
-        if [[ $MONITOR_COUNT -gt 1 ]]; then
+    elif [[ $MONITOR_COUNT -gt 1 ]]; then
+        echo "Workspace configuration"
+        echo "1) Groob"
+        echo "2) Sol"
+        read -rp $'Pick an option 1-2\n' choice
+
+        # Groob
+        if [[ $choice -eq 1 ]]; then
             for i in {1..5}; do
                 config+="workspace = $i, monitor:${MONITORS[0]}, default:true"$'\n'
             done
+
             for i in {6..10}; do
                 config+="workspace = $i, monitor:${MONITORS[1]}, default:true"$'\n'
             done
+        fi
+
+        # Sol
+        if [[ $choice -eq 2 ]]; then
+            if [[ $MONITOR_COUNT -eq 2 ]]; then
+                for i in {1..3}; do
+                    config+="workspace = $i, monitor:${MONITORS[0]}, default:true"$'\n'
+                done
+
+                for i in {4..6}; do
+                    config+="workspace = $i, monitor:${MONITORS[1]}, default:true"$'\n'
+                done
+
+                for i in {7..9}; do
+                    config+="workspace = $i"$'\n'
+                done
+
+            elif [[ $MONITOR_COUNT -gt 2 ]]; then
+                for in {1..2}; do
+                    config+="workspace = $i, monitor:${MONITORS[0]}, default:true"$'\n'
+                done
+
+                for i in {3..4}; do
+                    config+="workspace = $i, monitor:${MONITORS[1]}, default:true"$'\n'
+                done
+
+                for i in {5..6}; do
+                    config+="workspace = $i, monitor:${MONITORS[2]}, default:true"$'\n'
+                done
+
+                for i in {7..9}; do
+                    config+="workspace = $i"$'\n'
+                done
+            fi
         else
-            for i in {1..10}; do
-                config+="workspace = $i"$'\n'
-            done
+            echo -e "Please pick from a valid option"
         fi
     fi
 
-    # Sol config
-    if [[ $choice -eq 2 ]]; then
-        if [[ $MONITOR_COUNT -gt 1 ]]; then
-            for i in {1..2}; do
-                config+="workspace = $i, monitor:${MONITORS[0]}, default:true"$'\n'
-            done
-            for i in {3..4}; do
-                config+="workspace = $i, monitor:${MONITORS[1]}, default:true"$'\n'
-            done
-            for i in {5..6}; do
-                config+="workspace = $i, monitor:${MONITORS[2]}, default:true"$'\n'
-            done
-            for i in {7..9}; do
-                config+="workspace = $i"$'\n'
-            done
-        else
-            for i in {1..10}; do
-                config+="workspace = $i"$'\n'
-            done
-        fi
-    fi
     printf "%s" "$config"
+
 }
 
 generate_workspace_config
